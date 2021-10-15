e13c66b1b0744fc72584f15a0f13fb577a5c92fe
Elina <20183ti015@utez.edu.mx>
2021-10-15 00:11:05 -0500

    Prueba con url repo para elementos de cache v4

--- sw.js
+++ sw.js
@@ -8,8 +8,8 @@ let url = _window.location.href;
 self.addEventListener('install', (event) => {
     console.log('SW: Instalado');
 
-    const staticCache = caches.open(CACHE_STATIC_NAME).then((cache) => {
-        //if(url.includes('localhost')){
+    /*const staticCache = caches.open(CACHE_STATIC_NAME).then((cache) => {
+        if(url.includes('localhost')){
             return cache.addAll([
                 '/',
                 'index', //si pongo index.html no me lo reconoce
@@ -19,8 +19,18 @@ self.addEventListener('install', (event) => {
                 'images/noticia4.png',
                 'js/app.js'
             ]);
-        
-    });
+        }else{
+            return cache.addAll([
+                '/',
+                'index',
+                'images/noticia1.png',
+                'images/noticia2.png',
+                'images/noticia3.png',
+                'images/noticia4.png',
+                'js/app.js'
+            ]);
+        }
+    });*/
 
     const inmutableCache = caches.open(CACHE_INMUTABLE_NAME).then((cache) => {
         return cache.addAll([
@@ -30,8 +40,8 @@ self.addEventListener('install', (event) => {
         ]);
     });
 
-
-    event.waitUntil(Promise.all([staticCache, inmutableCache]));
+    event.waitUntil(inmutableCache)
+    //event.waitUntil(Promise.all([staticCache, inmutableCache]));
 });
 
 self.addEventListener('fetch', (event) => {
