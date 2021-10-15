9b1495c53b0b6fe38d7e8615357fbc0eb4b5a30e
Elina <20183ti015@utez.edu.mx>
2021-10-15 00:03:00 -0500

    Prueba con url repo para elementos de cache

--- sw.js
+++ sw.js
@@ -1,3 +1,68 @@
-self.addEventListener('install',(event)=>{
+const CACHE_STATIC_NAME = 'staticCache-v1';
+const CACHE_DYNAMIC_NAME = 'dynamicCache-v1';
+const CACHE_INMUTABLE_NAME = 'inmutableCache-v1';
+
+var _window = this || self || window;
+let url = _window.location.href;
+
+self.addEventListener('install', (event) => {
     console.log('SW: Instalado');
+
+    const staticCache = caches.open(CACHE_STATIC_NAME).then((cache) => {
+        if(url.includes('localhost')){
+            return cache.addAll([
+                '/',
+                'index', //si pongo index.html no me lo reconoce
+                'images/noticia1.png',
+                'images/noticia2.png',
+                'images/noticia3.png',
+                'images/noticia4.png',
+                'js/app.js'
+            ]);
+        }else{
+            return cache.addAll([
+                '/',
+                '/PWA-U2-T1-EECM/index', //si pongo index.html no me lo reconoce
+                '/PWA-U2-T1-EECM/images/noticia1.png',
+                '/PWA-U2-T1-EECM/images/noticia2.png',
+                '/PWA-U2-T1-EECM/images/noticia3.png',
+                '/PWA-U2-T1-EECM/images/noticia4.png',
+                '/PWA-U2-T1-EECM/js/app.js'
+            ]);
+        }
+    });
+
+    const inmutableCache = caches.open(CACHE_INMUTABLE_NAME).then((cache) => {
+        return cache.addAll([
+            'https://cdn.jsdelivr.net/npm/bootstrap@4.6.0/dist/css/bootstrap.min.css',
+            'https://code.jquery.com/jquery-3.5.1.min.js',
+            'https://cdn.jsdelivr.net/npm/bootstrap@4.6.0/dist/js/bootstrap.bundle.min.js'
+        ]);
+    });
+
+
+    event.waitUntil(Promise.all([staticCache, inmutableCache]));
 });
+
+self.addEventListener('fetch', (event) => {
+    console.log(event.request.url);
+
+    const respuestaCache = caches.match(event.request).then((resp) =>{
+        if(resp){
+            return resp;
+        }
+        console.log('No está en caché', event.request.url);
+
+        return fetch(event.request).then((respuestaNet) => {
+            event.preventDefault()
+            console.log(event.request);
+            caches.open(CACHE_DYNAMIC_NAME).then((cache) => {
+                cache.put(event.request, respuestaNet)
+            });
+
+            return respuestaNet.clone()
+        })
+    })
+
+    event.respondWith(respuestaCache)
+})
